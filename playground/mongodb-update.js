3601fed17ec9a98f775d0189303f0159dc31a7e4
fercon997 <fercon997@gmail.com>
2017-10-21 10:39:09 -0400

    Added update functions to playground

---
+++ playground/mongodb-update.js
@@ -0,0 +1,24 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectId} = require('mongodb');
+
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) return console.log("Unable to connect to MongoDB server");
+  console.log("Connected to MongoDB server");
+  /*db.collection('Todos').findOneAndUpdate({
+    _id: new ObjectId("59eb55e70eb663adb33d57bb")
+  }, {
+    $set: {
+      completed: true
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => console.log(result));*/
+  db.collection('Users').findOneAndUpdate({name: "Fernando"}, {
+    $set: {
+      location: "Caracas"
+    }
+  }, {returnOriginal: false}).then((result) => console.log(result));
+  //db.close();
+});
