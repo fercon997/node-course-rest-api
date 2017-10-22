7d24c6654119fb8f139d10c19c5bcb9ab89ac70e
fercon997 <fercon997@gmail.com>
2017-10-22 14:16:41 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectId} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -21,6 +23,16 @@ app.get('/todos', (req,res) => {
   Todo.find().then((todos) => {
     res.send({todos});
   }, (e) => res.status(400).send(e));
+});
+
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  console.log("id: ",id);
+  if (!ObjectId.isValid(id)) return res.status(404).send();
+  Todo.findById(id).then((todo) => {
+    if(!todo) return res.status(404).send();
+    res.send({todo});
+  }).catch((e) => res.status(400).send());
 });
 
 app.listen(3000, () => console.log("Started on port 3000"));
