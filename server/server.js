24e3371eb2dff964895185bf6096a6cf3fbc3458
fercon997 <fercon997@gmail.com>
2017-10-22 08:50:26 -0400

    modified server.js

--- server/server.js
+++ server/server.js
@@ -5,13 +5,27 @@ mongoose.connect('mongodb://localhost:27017/TodoAPP');
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minLength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: true
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
+  }
+});
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minLength: 1,
+    trim: true
   }
 });
 
@@ -19,12 +33,15 @@ var Todo = mongoose.model('Todo', {
   text: 'Cook dinner'
 });
 newTodo.save().then((doc) => console.log("Saved todo",doc),
+(e) => console.log("Unable to save todo"));
+
+var otherTodo = new Todo({});
+otherTodo.save().then((doc) => console.log(JSON.stringify(doc, undefined, 2)),
 (e) => console.log("Unable to save todo"));*/
 
-var otherTodo = new Todo({
-  text: 'Play fifa',
-  completed: false,
-  completedAt: 123
+var newUser = new User({
+
 });
-otherTodo.save().then((doc) => console.log(JSON.stringify(doc, undefined, 2)),
-(e) => console.log("Unable to save todo"));
+
+newUser.save().then((doc) => console.log('User saved ',doc),
+(e) => console.log('Unable to save user',e));
